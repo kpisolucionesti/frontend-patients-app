af07b6dbdee8ad4c2fc13cfe38c48e8e004db2c2
kpisolucionesti <kpisolucionesti@gmail.com>
2023-09-04 10:37:23 -0400

    Cambios
    Cambios en documentos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import RoomTable from './Components/Table/RoomTable';
 import TablePatients from './Components/Table/TablePatients';
-import Layout from './newComponents/Layouts/Layout';
 
 function App() {
   return (
